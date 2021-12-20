0a9dc2897708cc1ac55a7179d91b1364383beae0
lsh <tlgj255@naver.com>
2021-12-20 22:09:30 +0900

    first commit

---
+++ src/main/kotlin/com/example/chrismas/ChrismasApplication.kt
@@ -0,0 +1,11 @@
+package com.example.chrismas
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ChrismasApplication
+
+fun main(args: Array<String>) {
+    runApplication<ChrismasApplication>(*args)
+}
---
+++ src/test/kotlin/com/example/chrismas/ChrismasApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.chrismas
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ChrismasApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
