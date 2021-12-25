c1a58bc96e363797aa76328fb920b968fd0d338a
lsh <tlgj255@naver.com>
2021-12-25 22:19:42 +0900

    Add : 인증 get post delete put 추가

--- src/test/kotlin/DefaultTestSetting.kt
+++ src/test/kotlin/DefaultTestSetting.kt
@@ -1,3 +1,4 @@
+import com.example.christmas.config.JwtTokenConfig
 import org.hamcrest.Matchers
 import org.hamcrest.core.IsNull
 import org.springframework.beans.factory.annotation.Autowired
@@ -13,11 +14,18 @@ abstract class DefaultTestSetting {
 
     abstract val rootPath: String
 
-    private var _mockMvc: MockMvc? = null
+    private lateinit var _mockMvc: MockMvc
+
+    private lateinit var _jwtTokenConfig: JwtTokenConfig
 
     @Autowired
-    fun setMockMvc(mockMvc: MockMvc?) {
+    fun setMockMvc(mockMvc: MockMvc) {
         this._mockMvc = mockMvc
+    }
+
+    @Autowired
+    fun setJwtTokenConfig(jwtTokenConfig: JwtTokenConfig) {
+        this._jwtTokenConfig = jwtTokenConfig
     }
 
     fun ResultActions.checkIsError(status: ResultMatcher, errorMessage: String) =
@@ -34,31 +42,62 @@ abstract class DefaultTestSetting {
 
 
     fun doGet(uri: String = "", param: String = "", root: String = rootPath) =
-        _mockMvc!!.perform(
+        _mockMvc.perform(
             MockMvcRequestBuilders.get("$root/$uri$param")
                 .accept(MediaType.APPLICATION_JSON)
         )
 
+    fun doAuthenticatedGet(token: String = "", uri: String = "", param: String = "", root: String = rootPath) =
+        _mockMvc.perform(
+            MockMvcRequestBuilders.get("$root/$uri$param")
+                .accept(MediaType.APPLICATION_JSON)
+                .header(_jwtTokenConfig.header, "Bearer $token")
+        )
+
     fun doPost(uri: String = "", body: String = "", root: String = rootPath) =
-        _mockMvc!!.perform(
+        _mockMvc.perform(
             MockMvcRequestBuilders.post("$root/$uri")
                 .accept(MediaType.APPLICATION_JSON)
                 .contentType(MediaType.APPLICATION_JSON)
                 .content(body)
         )
 
+    fun doAuthenticatedPost(token: String = "", uri: String = "", body: String = "", root: String = rootPath) =
+        _mockMvc.perform(
+            MockMvcRequestBuilders.post("$root/$uri")
+                .accept(MediaType.APPLICATION_JSON)
+                .contentType(MediaType.APPLICATION_JSON)
+                .header(_jwtTokenConfig.header, "Bearer $token")
+                .content(body)
+        )
+
     fun doDelete(uri: String = "", root: String = rootPath) =
-        _mockMvc!!.perform(
+        _mockMvc.perform(
             MockMvcRequestBuilders.delete("$root/$uri")
                 .accept(MediaType.APPLICATION_JSON)
         )
 
+    fun doAuthenticatedDelete(token: String = "", uri: String = "", root: String = rootPath) =
+        _mockMvc.perform(
+            MockMvcRequestBuilders.delete("$root/$uri")
+                .accept(MediaType.APPLICATION_JSON)
+                .header(_jwtTokenConfig.header, "Bearer $token")
+        )
 
     fun doPut(uri: String = "", body: String = "", root: String = rootPath) =
-        _mockMvc!!.perform(
+        _mockMvc.perform(
             MockMvcRequestBuilders.put("$root/$uri")
                 .accept(MediaType.APPLICATION_JSON)
                 .contentType(MediaType.APPLICATION_JSON)
+                .content(body)
+        )
+
+    fun doAuthenticatedPut(token: String = "", uri: String = "", body: String = "", root: String = rootPath) =
+        _mockMvc.perform(
+            MockMvcRequestBuilders.put("$root/$uri")
+                .accept(MediaType.APPLICATION_JSON)
+                .contentType(MediaType.APPLICATION_JSON)
+                .header(_jwtTokenConfig.header, "Bearer $token")
                 .content(body)
         )
 }
