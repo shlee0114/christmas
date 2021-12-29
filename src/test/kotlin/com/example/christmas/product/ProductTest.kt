9653776d9a29109269311e0040083a139d422402
lsh <tlgj255@naver.com>
2021-12-29 22:15:23 +0900

    Add : product test code

--- src/test/kotlin/com/example/christmas/product/ProductTest.kt
+++ src/test/kotlin/com/example/christmas/product/ProductTest.kt
@@ -1,11 +1,16 @@
 package com.example.christmas.product
 
 import DefaultTestSetting
+import com.example.christmas.utils.ErrorMessage
+import org.hamcrest.core.IsNull
 import org.junit.jupiter.api.MethodOrderer
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.TestMethodOrder
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.web.servlet.result.MockMvcResultHandlers
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers.*
+import org.hamcrest.Matchers.`is`
 
 @SpringBootTest
 @AutoConfigureMockMvc
@@ -15,16 +20,27 @@ class ProductTest : DefaultTestSetting() {
 
     @Test
     fun productList_returnProductList_success() {
-
+        doGet()
+            .andDo(MockMvcResultHandlers.print())
+            .andExpect(status().isOk)
+            .andExpect(jsonPath("$.success", `is`(true)))
+            .andExpect(jsonPath("$.data").isArray)
+            .andExpect(jsonPath("$.error", `is`(IsNull.nullValue())))
     }
 
     @Test
     fun product_returnProduct_success() {
-
+        doGet("1")
+            .andDo(MockMvcResultHandlers.print())
+            .andExpect(status().isOk)
+            .andExpect(jsonPath("$.success", `is`(true)))
+            .andExpect(jsonPath("$.data").exists())
+            .andExpect(jsonPath("$.error", `is`(IsNull.nullValue())))
     }
 
     @Test
     fun product_throwException_notFound() {
-
+        doGet("99999")
+            .checkIsError(status().isBadRequest, ErrorMessage.NOT_FOUND, "99999")
     }
 }
