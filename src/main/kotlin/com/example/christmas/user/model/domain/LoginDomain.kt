2288e3c092a710d7ac40fb02a08b05640e821312
seonghyunlee <tlgj255@naver.com>
2021-12-31 00:04:13 +0900

    Add : LoginRequest -> LoginDomain 변환 추가

--- src/main/kotlin/com/example/christmas/user/model/domain/LoginDomain.kt
+++ src/main/kotlin/com/example/christmas/user/model/domain/LoginDomain.kt
@@ -1,16 +1,19 @@
 package com.example.christmas.user.model.domain
 
+import com.example.christmas.user.model.request.LoginRequest
 import javax.persistence.*
 
 @Entity(name = "user")
 data class LoginDomain(
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    val seq : Long,
-    val id : String,
-    val pw : String,
-    val userType : Char = '0'
+    val seq: Long = 0,
+    val id: String,
+    val pw: String,
+    val userType: Char = '0'
 ) {
+    constructor(login: LoginRequest, type: Char) : this(id = login.id, pw = login.password, userType = type)
+
     override fun equals(other: Any?) = super.equals(other)
     override fun hashCode(): Int = javaClass.hashCode()
     override fun toString() = super.toString()
