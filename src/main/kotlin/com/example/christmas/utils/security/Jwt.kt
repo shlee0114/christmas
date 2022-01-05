07e1c425c165d3743597e0d4e9eee2c9bdd50987
seonghyunlee <tlgj255@naver.com>
2022-01-05 22:31:41 +0900

    Add : jwt create, verify 모듈 추가

---
+++ src/main/kotlin/com/example/christmas/utils/security/Jwt.kt
@@ -0,0 +1,70 @@
+package com.example.christmas.utils.security
+
+import com.auth0.jwt.JWT
+import com.auth0.jwt.JWTCreator
+import com.auth0.jwt.JWTVerifier
+import com.auth0.jwt.algorithms.Algorithm
+import com.auth0.jwt.interfaces.DecodedJWT
+import java.util.*
+
+class Jwt(
+    private val issuer: String,
+    private val clientSecret: String,
+    private val expirySeconds: Int,
+    private val algorithm: Algorithm,
+    private val jwtVerifier: JWTVerifier
+) {
+
+    constructor(issuer: String, clientSecret: String, expirySeconds: Int) : this(
+        issuer = issuer,
+        clientSecret = clientSecret,
+        expirySeconds = expirySeconds,
+        algorithm = Algorithm.HMAC512(clientSecret),
+        jwtVerifier = JWT.require(Algorithm.HMAC512(clientSecret))
+            .withIssuer(issuer)
+            .build()
+    )
+
+    fun create(claims: Claims) =
+        JWT.create()
+            .withIssuer(issuer)
+            .withIssuedAt(Date())
+            .withExpiresAt()
+            .withClaim("userId", claims.userId)
+            .withClaim("userName", claims.userName)
+            .withClaim("roles", claims.roles)
+            .sign(algorithm)
+
+    private fun JWTCreator.Builder.withExpiresAt() =
+        if(expirySeconds > 0)
+            withExpiresAt(Date(Date().time + expirySeconds * 1_000L))
+        else
+            this
+
+    fun verify(token: String) =
+        Claims(jwtVerifier.verify(token))
+
+    data class Claims(
+        val userId: String,
+        val userName: String,
+        val roles: List<String>,
+        val iat: Date? = null,
+        val exp: Date? = null
+    ) {
+        constructor(decodedJWT: DecodedJWT) : this(
+            userId = decodedJWT.getClaim("userId").asString(),
+            userName = decodedJWT.getClaim("userName").asString(),
+            roles = decodedJWT.getClaim("roles").asList(String::class.java),
+            iat = decodedJWT.issuedAt,
+            exp = decodedJWT.expiresAt,
+        )
+
+        companion object {
+            fun of(userId: String, userName: String, roles: List<String>) =
+                Claims(userId, userName, roles)
+        }
+
+        override fun equals(other: Any?) = super.equals(other)
+        override fun hashCode() = super.hashCode()
+    }
+}
